a3ab3ffb0279777f9427a26a1bef4f6ecca11a11
Neşe Nefise Çiftçioğlu <94238138+Nese-Nefise-Ciftcioglu@users.noreply.github.com>
2023-01-17 23:39:18 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+/*
+Run these commands:
+
+java -jar java-cup-11b.jar -parser MyParser -symbols MySymbol < MyParser.cup
+java -jar jflex-1.8.2.jar MyScanner.lex
+javac *.java
+java Main example.txt
+*/
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+class Main {
+	public static void main(String[] args) throws FileNotFoundException {
+		String fileName = "example.txt";
+		MyParser parser = new MyParser(new MyScanner(new FileReader(fileName)));
+		try {
+			parser.parse();
+			//parser.debug_parse();
+		}
+		catch (Exception e) {
+			System.out.println("Caught an exception.");
+		}
+	}
+}
+
